025107d6a0a833121bd5f7c27525b5f09876d7e8
Caleb Miller <caleb10miller@gmail.com>
2025-04-09 23:45:33 -0400

    remove multicollinearity in long format

--- drop_multicollinearity.py
+++ drop_multicollinearity.py
@@ -4,6 +4,7 @@ import numpy as np
 df = pd.read_csv("data/final_data/nfl_stats_long_format.csv") 
 
 df['Yards per Completion'] = df['Passing Yards'] / df['Passing Completions']
+
 df["Attempts per Completion"] = np.where(
     df["Passing Attempts"] > 0,
     df["Passing Completions"] / df["Passing Attempts"],
@@ -19,15 +20,22 @@ df["Yards per Reception"] = np.where(
     df["Receiving Yards"] / df["Receptions"],
     np.nan)
 
+df['Total Touchdowns Allowed'] = df['Passing Touchdowns Allowed'] + df['Rushing Touchdowns Allowed']
+
+df['Special Teams Impact'] = df['ST_Sacks'] + df['ST_Interceptions'] + df['ST_Fumble Recoveries'] + df['ST_Forced Fumbles']
+
+
 columns_to_keep = [
-    "Age", "Games Played", "Games Started", "Yards per Completion", "Attempts per Completion",
+    "Player Name", "Age", "Season", "Position", "Team",
+    "Games Played","Games Started","Yards per Completion", "Attempts per Completion",
     "Passing Touchdowns", "Interceptions Thrown", "Rushing Attempts",
     "Yards per Carry", "Rushing Touchdowns", "Targets", 'Yards per Reception',
-    "Receiving Touchdowns", "Fumbles" , "XP2", "ESPN ADP", "Sleeper ADP", "NFL ADP",
-    "RTSports ADP", "Average ADP", "Positional ADP"
+    "Receiving Touchdowns", "Fumbles","Field Goals Made","Extra Points Made",
+    "Total Touchdowns Allowed","Special Teams Impact","ST_Safeties",
+    "ST_Special Teams Touchdowns","XP2","Average ADP","Positional ADP",
+    "PPR Fantasy Points Scored","Standard Fantasy Points Scored"
 ]
 
 df_filtered = df[columns_to_keep].copy()
 
-df_filtered.to_csv("data/final_data/nfl_stats_long_format_.csv", index=False)
-
+df_filtered.to_csv("data/final_data/nfl_stats_long_format_filtered.csv", index=False)
